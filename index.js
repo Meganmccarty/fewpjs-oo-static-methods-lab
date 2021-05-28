bdc370e638b8bea082d5578faf310939b34a4de9
Megan McCarty <megan_mccarty@hotmail.com>
2021-05-28 16:43:53 -0400

    commit tree be83e8b847bdbaddfdd406aa08f027c331252496

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    static sanitize(string) {
+        return string.replace( /[^A-Za-z0-9 '-]/g, '' );
+    }
+    
+    static titleize(string) {
+        const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+        const wordsArray = string.split(' ');
+        let newString = [];
+
+        for(let i = 0; i < wordsArray.length; i++) {
+            if (i === 0 || !exceptions.includes(wordsArray[i])) {
+                newString.push(this.capitalize(wordsArray[i]))
+            } else {
+                newString.push(wordsArray[i])
+            }
+        }
+        
+        return newString.join(' ') 
+    }
 }
+
+console.log(Formatter.titleize('how to become a software engineer'));
